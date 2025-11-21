90b98486d03968582ca8665eea1d246e57d25cbd
Akashkatageri <d.katageri360@gmail.com>
2025-11-21 15:22:02 +0530

    initial commit

---
+++ 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int even_add(int num)(
+    return(num%2 ==0)?printf("Even \n") : printf("Odd \n");
+
+)
+
+int main()(
+    even_odd(5);
+
+    int a =10;
+    int b = ++a;
+    printf("%d",b);
+    return 0;
+)
